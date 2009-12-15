f011d929d92d717031029163335abe7601f10bf1
Sebastian Wohlrab <sebastian.wohlrab@gmail.com>
2009-12-15 09:47:48 +0100

    added source

---
+++ src/sample.js
@@ -0,0 +1,27 @@
+/**
+ * Additional Options will be introduced by ^ tag.
+ * Multiple options must be seperated by ,
+ * Options must be in format attribute=value
+ * attribute must be a valid html attribute for the specific tag, expecting the "text" attribute (which will be the innerHTML
+ */
+
+// Example:
+
+window.addEvent('domready', function() {
+    // Generating Header
+    document.body.expandAppr( 'div#header>div#logo^text=Containing Logo+div#navigation+div#subnavigation' );
+
+    // the Navigation with some li's and a's
+    $('navigation').expandAppr( 'ul>li*3>a^href=http://www.nose.ch,text=NOSE,target=_blank,title=a navigation link' );
+
+    // the Navigation with some li's and a's
+    $('subnavigation').expandAppr( 'ul>li*5>a^href=http://www.spiegel.de,text=Spiegel,title=a link' );
+
+    // Generating Main Content Body with left and right column
+    document.body.expandAppr( 'div#main>div#content' );
+    $('content').expandAppr( 'div.box*24>h3^text=a little box+p^text=with some content' );
+    $('content').expandAppr( 'br^style=clear:both' );
+
+    // Generating Footer and Sitemap Body
+    document.body.expandAppr( 'div#bottom>div#sitemap^text=The Sitemap+div#footer^text=The Footer' );
+});
---
+++ src/zencode.js
@@ -0,0 +1,156 @@
+/**
+ * (Zen) Extent Appreviation for Mootools 1.2
+ * by Sebastian Wohlrab @ NOSE AG Design Intelligence <sebastian.wohlrab@nose.ch>
+ */
+
+Element.implement("expandAppr", function( str ) {
+    var parent = this;
+
+    if ( str.search('>') > 0 )
+    {
+        var nodes = str.split('>');
+
+        for ( var i = 0; i < nodes.length; i++ )
+        {
+            if ( nodes[i].split('+').length > 1 )
+            {
+                var subnodes = nodes[i].split('+');
+                for ( var j = 0; j < subnodes.length; j++ )
+                {
+                    var newEl = createElement( subnodes[j] )
+                    adoptNew(parent, newEl);
+                }
+                parent = newEl;
+            }
+            else {
+                var newEl = createElement( nodes[i] );
+                adoptNew(parent, newEl);
+                parent = newEl;
+            }
+        }
+    }
+    else {
+        parent.adopt( createElement( str ) );
+    }
+    function adoptNew( parent, element )
+    {
+        if ( parent.length > 1 )
+        {
+            for ( var i = 0; i < parent.length; i++ )
+            {
+                if ( element.length > 1 )
+                {
+                    for ( var j = 0; j < element.length; j++ )
+                    {
+                        parent[i].adopt( element[j].clone() );
+                    }
+                }
+                else {
+                    parent[i].adopt( element.clone() );
+                }
+            }
+        }
+        else {
+            if ( element.length > 1 )
+            {
+                for ( var i = 0; i < element.length; i++ )
+                {
+                    parent.adopt( element[i] );
+                }
+            }
+            else {
+                parent.adopt( element );
+            }
+        }
+    }
+    function createElement( str )
+    {
+        if ( str.split("^").length > 1 ) {
+            var additional = str.split("^");
+            str = additional[0];
+            additional = additional[1];
+        }
+
+        if ( str.split("#").length > 1 )
+        {
+            var elementArr = str.split("#");
+            var id = elementArr[1];
+            if ( id.split(".").length > 1 )
+            {
+                var classes = id.split(".");
+                id = classes.shift();
+            }
+            var element = new Element( elementArr[0] );
+        }
+        else {
+            if ( str.split('*').length > 1 )
+            {
+                var strIterate = str.split('*');
+                str = strIterate[0];
+                strIterate.shift();
+            }
+            if ( str.split('.').length > 1 )
+            {
+                var classes = str.split('.');
+                var element = new Element( classes.shift() );
+            }
+            else {
+                var element = new Element( str );
+            }
+        }
+        
+        if ( element.get('tag') == 'a' )
+        {
+            element.set('href', '#');
+        }
+        if ( additional ) {
+            addAditional( element, additional );
+        }
+
+        if ( id )
+        {
+            element.set('id', id );
+        }
+        if ( classes )
+        {
+            var classstr = "";
+            for ( var i = 0; i < classes.length; i++ )
+            {
+                classstr += " " + classes[i];
+            }
+            classstr = classstr.substr(1, classstr.length);
+            element.set('class', classstr );
+        }
+        if ( strIterate )
+        {
+            var elements = [];
+            for ( var i = 0; i < strIterate; i++ )
+            {
+                elements.push( element.clone() );
+            }
+        }
+        if ( elements )
+        {
+            return elements;
+        }
+        else {
+            return element;
+        }
+    }
+    function addAditional( element, additional ) {
+        if ( additional.search(',') > 0 )
+        {
+            var additionals = additional.split(',');
+            
+            for ( var i = 0; i < additionals.length; i++ )
+            {
+                var segment = additionals[i].split('=');
+                element.set(segment[0], segment[1]);
+            }
+        }
+        else {
+            var segment = additional.split('=');
+            element.set(segment[0], segment[1]);
+        }
+    }
+});
