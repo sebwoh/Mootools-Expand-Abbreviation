779923ef4f29345f1132fe03f175b49cb29443a4
Sebastian Wohlrab <sebastian.wohlrab@gmail.com>
2009-12-15 17:13:06 +0100

    Fixed syntax in example file :)

--- Source/ExpandAbbreviation.example.js
+++ Source/ExpandAbbreviation.example.js
@@ -19,19 +19,19 @@ provides: [Element.expandAbbr]
 
 window.addEvent('domready', function() {
     // Generating Header
-    document.body.expanAbbr( 'div#header>div#logo^text=Containing Logo+div#navigation+div#subnavigation' );
+    document.body.expandAbbr( 'div#header>div#logo^text=Containing Logo+div#navigation+div#subnavigation' );
 
     // the Navigation with some li's and a's
-    $('navigation').expanAbbr( 'ul>li*3>a^href=http://www.google.com,text=GOOGLE,target=_blank,title=a navigation link' );
+    $('navigation').expandAbbr( 'ul>li*3>a^href=http://www.google.com,text=GOOGLE,target=_blank,title=a navigation link' );
 
     // the Navigation with some li's and a's
-    $('subnavigation').expanAbbr( 'ul>li*5>a^href=http://www.nyt.com,text=NYT,title=a link' );
+    $('subnavigation').expandAbbr( 'ul>li*5>a^href=http://www.nyt.com,text=NYT,title=a link' );
 
     // Generating Main Content Body with left and right column
-    document.body.expanAbbr( 'div#main>div#content' );
-    $('content').expanAbbr( 'div.box*24>h3^text=a little box+p^text=with some content' );
-    $('content').expanAbbr( 'br^style=clear:both' );
+    document.body.expandAbbr( 'div#main>div#content' );
+    $('content').expandAbbr( 'div.box*24>h3^text=a little box+p^text=with some content' );
+    $('content').expandAbbr( 'br^style=clear:both' );
 
     // Generating Footer and Sitemap Body
-    document.body.expanAbbr( 'div#bottom>div#sitemap^text=The Sitemap+div#footer^text=The Footer' );
+    document.body.expandAbbr( 'div#bottom>div#sitemap^text=The Sitemap+div#footer^text=The Footer' );
 });
